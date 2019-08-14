def850a23802423d7bcc3fad6ba585ba5f7a39f4
Chen Wang <wangcees@gmail.com>
2019-08-14 18:28:35 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, s)
+  array.push(s)
+end
+
+def using_unshift(array, s)
+  array.unshift(s)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array,arr)
+  array.concat(arr)
+end
+
+def using_insert(array, element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten()
+end
+
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, index)
+  array.delete_at(index)
+end
